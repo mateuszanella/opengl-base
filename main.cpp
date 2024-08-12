a661cffc6afe8a7a2dbccb37a9e6550a0e505127
mateuszanella <mateusblk1@gmail.com>
2024-08-12 19:29:25 -0300

    Triangle

--- main.cpp
+++ main.cpp
@@ -51,12 +51,14 @@ int main()
         glTranslatef(0.0f, 0.0f, 0.0);
 
 		glBegin(GL_TRIANGLES);
-				glColor3f(1.0f, 0.0f, 0.0f);
-				glVertex3f(-0.5f, -0.5f, 0.0f); 
-				glColor3f(0.0f, 1.0f, 0.0f); 
-				glVertex3f(0.5f, -0.5f, 0.0f);  
-				glColor3f(0.0f, 0.0f, 1.0f); 
-				glVertex3f(0.0f,  0.5f, 0.0f);
+			glColor3f(1.0f, 0.0f, 0.0f);
+			glVertex3f(-0.5f, -0.5f, 0.0f); 
+
+			glColor3f(0.0f, 1.0f, 0.0f);
+			glVertex3f(0.5f, -0.5f, 0.0f);  
+
+			glColor3f(0.0f, 0.0f, 1.0f);
+			glVertex3f(0.0f, 0.5f, 0.0f);
 		glEnd();
 
         glPopMatrix();
